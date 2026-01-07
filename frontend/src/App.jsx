87060db4cba829d0d9adacd3476bf256d15c6554
Linda Jensen <linda.jensen81@me.com>
2026-01-07 15:07:27 +0100

    Add React Router setup with MainLayout and HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      {/* <Route path="/about" element={<About />} /> */}
+    </Route>
+  )
+);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
