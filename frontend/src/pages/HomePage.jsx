3576862d7ed7201dfd314fc31614b60dd028b1ce
Linda Jensen <linda.jensen81@me.com>
2026-01-07 14:43:57 +0100

    Create HomePage with HowItWorks section

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import HowItWorks from "../components/HowItWorks";
+
+function HomePage() {
+  return (
+    <>
+      {/* <Hero /> */}
+      <HowItWorks />
+    </>
+  );
+}
+
+export default HomePage;
