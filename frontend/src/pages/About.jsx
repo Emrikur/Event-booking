8914f7bd259c70638d4fc151c5a8a1768058a5c5
Emrikur <151168924+Emrikur@users.noreply.github.com>
2026-01-08 14:59:01 +0100

    Add Mission section for About

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,12 +1,14 @@
 import AboutHero from '../components/AboutHero';
 import AboutMission from '../components/AboutMission';
+import AboutStats from '../components/AboutStats';
 
 function About() {
   return (
     <>
     <AboutHero/>
     <AboutMission/>
-      <h1>About Us</h1>
+    <AboutStats/>
+
     </>
   );
 };
