c7aa008ee88bea062d1f207163f47cc5d805d5c4
Emrikur <151168924+Emrikur@users.noreply.github.com>
2026-01-08 13:51:51 +0100

    Add Hero to about page

---
+++ frontend/src/components/AboutHero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import heroImg from "../assets/AboutHero.jpg";
+import "../styles/AboutHero.css";
+
+
+function AboutHero() {
+  return (
+    <section className="hero">
+       <img src={heroImg} alt="Hero" className="hero-image" />
+      <div className="hero-overlay">
+        <h1 className="hero-title">About EventHub</h1>
+        <p className="hero-text">We’re on a mission to bring people together through unforgettable experiences. EventHub makes it easy to discover, create, and share events that matter.</p>
+      </div>
+    </section>
+  );
+}
+
+export default AboutHero;
