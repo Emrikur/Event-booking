fc6b35a18aa84cfb9099f742b47cb45c5e23094e
Linda Jensen <linda.jensen81@me.com>
2026-01-08 10:19:52 +0100

    Include CallToAction in HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import HowItWorks from "../components/HowItWorks";
+import CallToAction from "../components/CallToAction";
 
 function HomePage() {
   return (
     <>
       {/* <Hero /> */}
       <HowItWorks />
+      <CallToAction />
     </>
   );
 }
